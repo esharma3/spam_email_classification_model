8ed6c3f10b3c329689f21eb2871885571043c940
Ekta Sharma <ekta.sharma3@gmail.com>
2020-06-20 00:46:45 -0500

    first tested version

---
+++ static/javascript/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+
+    else {
+          $('nav').removeClass('black');
+    }
+})
+
+
+// MDB Lightbox Init
+$(function () {
+      $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+      });
